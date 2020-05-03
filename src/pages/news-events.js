0452f608a783e99263843fb780aaf291621d468e
mostlyclicks <carlos@mostlyclicks.com>
2020-05-03 15:16:44 -0600

    layout for news events index

--- src/pages/news-events.js
+++ src/pages/news-events.js
@@ -3,6 +3,8 @@ import { Link, graphql } from "gatsby"
 import { RichText } from "prismic-reactjs"
 import styled from "styled-components"
 import Layout from "../components/layout"
+import { device } from "../components/media-queries"
+import SideBarAddress from "../components/Subnavs/address-sidebar"
 
 
 
@@ -12,15 +14,25 @@ const NewsEvents = ( {data} ) => {
 
   return (
     <Layout>
-      <h1>News & Events</h1>
-      {newsEvents.map(article => {
-        return (
-          <ArticleItem>
-            <h3><Link to={`/news-events/${article.node._meta.uid}`}>{RichText.asText(article.node.title)}</Link></h3>
-            <img src={article.node.thumbnail.url} alt={article.node.thumbnail.alt} />
-          </ArticleItem>
-        )
-      })}
+      <HeaderSection>
+        <h1>News & Events</h1>
+      </HeaderSection>
+      <NewsWrapper>
+        <NewsList>
+        {newsEvents.map(article => {
+          return (
+            <ArticleItem>
+              <h3><Link to={`/news-events/${article.node._meta.uid}`}>{RichText.asText(article.node.title)}</Link></h3>
+              <img src={article.node.thumbnail.url} alt={article.node.thumbnail.alt} />
+            </ArticleItem>
+          )
+        })}
+        </NewsList>
+        <L2Navigation>
+          <SideBarAddress />
+        </L2Navigation>
+      </NewsWrapper>
+
     </Layout>
   )
 }
@@ -48,9 +60,64 @@ export const query = graphql`
 }
 
 `
+const HeaderSection = styled.section`
+    padding:20px;
+    margin:0 auto;
+    width:100%;
+    h1 {
+      font-family:'IBM Plex Serif';
+    }
+    @media ${device.tablet} {
+
+      max-width:768px;
+    } 
+    @media ${device.laptop} {
+      max-width:960px;
+    }
+    @media ${device.laptopL} {
+      max-width:1200px;
+    }  
+`
+const NewsWrapper = styled.section`
+    width:100%;
+    margin:0 auto;
+  
+    display:grid;
+    grid-template-columns:1fr;
+    @media ${device.tablet} {
+      grid-template-columns:2fr 1fr;
+      max-width:768px;
+    } 
+    @media ${device.laptop} {
+      grid-template-columns:3fr 1fr;
+      max-width:960px;
+    }
+    @media ${device.laptopL} {
+      max-width:1200px;
+    }  
+`
+
+const NewsList = styled.div`
+    
+    @media ${device.tablet} {
+
+      max-width:768px;
+    } 
+    @media ${device.laptop} {
+      max-width:960px;
+    }
+    @media ${device.laptopL} {
+      max-width:1200px;
+    } 
+`
+
+const L2Navigation = styled.aside`
+
+`
+
 
 const ArticleItem = styled.div`
-  margin:20px 40px 20px 40px;
+  margin:20px 20px 20px 20px;
     display:flex;
     flex-direction:row;
     border-bottom:1px solid #ededed;
@@ -66,14 +133,12 @@ const ArticleItem = styled.div`
         color:var(--darkGray);
         text-decoration:none;
         padding:1rem;
-        
         &:hover {
           color:var(--orange);
           transition:.5s;
           background-color:var(--darkGray);
           border-bottom:3px solid var(--orange);
           margin-bottom:-3px;
-          
         }
       }
     }
