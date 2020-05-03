2af02af797d63e1487b7b55964a368bb5625c241
mostlyclicks <carlos@mostlyclicks.com>
2020-05-02 20:25:32 -0600

    add new awards image

--- src/pages/firm/awards-recognition.js
+++ src/pages/firm/awards-recognition.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import L2PagesLayout from "../../components/l2-page-template"
-import bgImage from "../../images/awards-recognition.jpg"
+import bgImage from "../../images/49534569656_d267bed785_o.jpg"
 
 const AwardsRecognition = () => (
   <>
