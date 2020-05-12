53fb5a3317a068bc61abd6dea8da73b3364a0bc8
mostlyclicks <carlos@mostlyclicks.com>
2020-05-12 00:52:13 -0600

    fix for prismic

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,12 @@
 const path = require(`path`);
+var fs = require("fs")
+var dir = "./.cache/caches/gatsby-source-prismic-graphql"
+
+exports.onPreBootstrap = () => {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir)
+  }
+}
 
 
 const wrapper = promise =>
