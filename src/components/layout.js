bfeebf6cef4f597ece034a670f6a9a21a1640a67
mostlyclicks <carlos@mostlyclicks.com>
2020-05-13 19:10:32 -0600

    add wb icon

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,11 +4,14 @@ import Header from "./header"
 import "./layout.css"
 import TopNav from "./top-nav"
 import Footer from "./footer"
+// import favicon from '../images/wieserbrothers-icon.png'
+// import Helmet from 'react-helmet'
    
 
 const Layout = ({ children }) => {
   return (
     <>
+      
       <div className="site">
         <TopNav />
         <Header />
