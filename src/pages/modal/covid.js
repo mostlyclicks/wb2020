c4b194d19b3eb28781d4b7c013fe2db0c80951b6
mostlyclicks <carlos@mostlyclicks.com>
2020-05-16 10:55:34 -0600

    fix covid typos

--- src/pages/modal/covid.js
+++ src/pages/modal/covid.js
@@ -10,9 +10,9 @@ const CovidPage = () => (
   <ConditionalLayout>
     
     <h1>Wieser Brothers COVID-19 Update</h1>
-    <p>As we continue to monitor the Coronavirus (COVID-19) and its rippling impact on our country, we want to ensure our clients, employees, subcontractors and suppliers that our 25+ year commitment to the industry remains stronger than ever. We are not shutting down our jobsites if work can be safely performed accordig to the CDC guidelines.</p>
+    <p>As we continue to monitor the Coronavirus (COVID-19) and its rippling impact on our country, we want to ensure our clients, employees, subcontractors and suppliers that our 25+ year commitment to the industry remains stronger than ever. We are not shutting down our jobsites if work can be safely performed according to the CDC guidelines.</p>
 
-    <p>During this time of uncertainty, Wieser Brothers is commited to deliver the construction services you depend on with the <strong>highest level of safety and efficiency and with minimul disruption.</strong> We will communicate any additional adjustment to our business operations.</p>
+    <p>During this time of uncertainty, Wieser Brothers is commited to deliver the construction services you depend on with the <strong>highest level of safety and efficiency and with minimal disruption.</strong> We will communicate any additional adjustment to our business operations.</p>
 
     <p>Please know that, as always, the health and safety of our employees, subcontractors, suppliers and their families is our highest priority, and we will remain diligent in our efforts to help prevent the spread of this illness.</p>
 
