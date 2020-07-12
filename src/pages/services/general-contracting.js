52ac76e6d6317a9aa016767c4f7ad546f43e51c1
mostlyclicks <carlos@mostlyclicks.com>
2020-07-11 18:23:04 -0600

    gen contracting working

--- src/pages/services/general-contracting.js
+++ src/pages/services/general-contracting.js
@@ -7,15 +7,20 @@ import { RichText } from "prismic-reactjs"
 const GeneralContracting = ({data}) => {
 
   const GeneralContractingData = data.prismic.allGeneral_contractings.edges[0]
-  console.log(GeneralContractingData.node)
+  console.log('hello')
+  console.log(GeneralContractingData.node.page_title[0].text)
+  console.log(GeneralContractingData.node.body[0].text)
+  console.log('goodbye')
   // const GenContContent = GeneralContractingData.node
 
   return (
   <>
     <L2PagesLayout 
-      title={`General Contracting`}
+      title={GeneralContractingData.node.page_title[0].text}
       subnav={`services`}
       backgroundImage={bgImage}
+      content={RichText.asText(GeneralContractingData.node.body)}
+
     //   content={`
     //   <p>As an industry leading general contractor, Wieser Brothers will manage all aspects of your construction project. We provide innovative solutions to your building project.</p>
     //   <p>In addition, we self-perform:</p>
@@ -30,15 +35,15 @@ const GeneralContracting = ({data}) => {
     // `}
     />
 
-    <div>df
-      {RichText.render(GeneralContractingData)}
+    <div>
+        {GeneralContractingData.node.body[0].text}
     </div>  
 
   </>
   )
 }
 
-export const GenContractin = graphql`
+export const GenContracting = graphql`
   query {
       prismic {
         allGeneral_contractings {
