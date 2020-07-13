8d77299e068b989cbbb233ed6b4d543a2830bba3
mostlyclicks <carlos@mostlyclicks.com>
2020-07-12 20:45:23 -0600

    take out comments from construction

--- src/pages/services/construction-management.js
+++ src/pages/services/construction-management.js
@@ -12,12 +12,6 @@ const ConstructionManagement = ({data}) => {
     subnav= {`services`}
     backgroundImage={bgImage}
     content = {ConstMgmntData.node.page_body}
-    // content = {`
-    //   <p>Wieser Brothers is a full-service general contractor, specializing in turn key solutions for our customers. 75% of our work is done on construction management concept.</p>
-
-    //   <p>We believe in not just building a great building, but building a continued relationship with our clients. In the end, we are partners in a project that you are proud to be a part of, one that will meet your needs <em><strong>now and into the future.</strong>
-    //   </em></p>         
-    // `}
   />
   )
 }
