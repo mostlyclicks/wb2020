bf2ec4a458943da9cd6e265bf5b8e6f67af1817d
mostlyclicks <carlos@mostlyclicks.com>
2021-05-15 11:46:58 -0600

    build all news pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,6 @@
 const path = require(`path`);
-var fs = require("fs")
+var fs = require("fs");
+const { createParameter } = require("typescript");
 var dir = "./.cache/caches/gatsby-source-prismic-graphql"
 
 exports.onPreBootstrap = () => {
@@ -12,7 +13,7 @@ exports.onPreBootstrap = () => {
 const wrapper = promise =>
   promise.then(result => {
     if (result.errors) {
-      throw result.erros
+      throw result.errors
     }
     return result
   });
@@ -35,16 +36,75 @@ exports.createPages = async ({ graphql, actions }) => {
             }
           }
 
-          allNews_and_eventss {
-            edges {
-              node {
-                title 
-                _meta {
-                  uid
-                }
-              }
-            }
+firstTen: allNews_and_eventss(sortBy: meta_firstPublicationDate_DESC, first: 10) {
+      totalCount
+      edges {
+        node {
+          title
+          _meta {
+            uid
           }
+          date_published
+        }
+        cursor
+      }
+    }
+    
+    
+    secondTen: allNews_and_eventss(sortBy: meta_firstPublicationDate_DESC, first: 10, after: "YXJyYXljb25uZWN0aW9uOjk=") {
+      edges {
+        node {
+          title
+          _meta {
+            uid
+          }
+          date_published
+        }
+        cursor
+      }
+    }
+    
+    
+    thirdTen: allNews_and_eventss(sortBy: meta_firstPublicationDate_DESC, first: 10, after: "YXJyYXljb25uZWN0aW9uOjE5") {
+      edges {
+        node {
+          title
+          _meta {
+            uid
+          }
+          date_published
+        }
+        cursor
+      }
+    }
+    
+    
+    fourTen: allNews_and_eventss(sortBy: meta_firstPublicationDate_DESC, first: 10, after: "YXJyYXljb25uZWN0aW9uOjI5") {
+      edges {
+        node {
+          title
+          _meta {
+            uid
+          }
+          date_published
+        }
+        cursor
+      }
+    }
+    
+    
+  	fiveTen: allNews_and_eventss(sortBy: meta_firstPublicationDate_DESC, first: 10, after: "YXJyYXljb25uZWN0aW9uOjM5") {
+      edges {
+        node {
+          title
+          _meta {
+            uid
+          }
+          date_published
+        }
+        cursor
+      }
+    }
 
           allTestimonials {
             edges {
@@ -63,7 +123,12 @@ exports.createPages = async ({ graphql, actions }) => {
   )
 
   const employmentOppsList = result.data.prismic.allEmployment_opportunitys.edges;
-  const newsEventsList = result.data.prismic.allNews_and_eventss.edges;
+  const newsEventsList1 = result.data.prismic.firstTen.edges;
+  const newsEventsList2 = result.data.prismic.secondTen.edges;
+  const newsEventsList3 = result.data.prismic.thirdTen.edges;
+  const newsEventsList4 = result.data.prismic.fourTen.edges;
+  const newsEventsList5 = result.data.prismic.fiveTen.edges;
+  
   const testimonialList = result.data.prismic.allTestimonials.edges;
 
   const empOppsTemplate = require.resolve(`./src/templates/empOps.js`)
@@ -84,17 +149,65 @@ exports.createPages = async ({ graphql, actions }) => {
     })
   })
 
-  newsEventsList.forEach(edge => {
-    createPage({
-      type: 'NewsEvent',
-      match: '/news-events/:uid',
-      path: `/news-events/${edge.node._meta.uid}`,
-      component: newsEventsTemplate,
-      context: {
-        uid: edge.node._meta.uid,
-      },
-    })
-  })
+   newsEventsList1.forEach(edge => {
+     createPage({
+       type: 'NewsEvent',
+       match: '/news-events/:uid',
+       path: `/news-events/${edge.node._meta.uid}`,
+       component: newsEventsTemplate,
+       context: {
+         uid: edge.node._meta.uid,
+       },
+     })
+   })
+
+   newsEventsList2.forEach(edge => {
+     createPage({
+       type: 'NewsEvent',
+       match: '/news-events/:uid',
+       path: `/news-events/${edge.node._meta.uid}`,
+       component: newsEventsTemplate,
+       context: {
+         uid: edge.node._meta.uid,
+       },
+     })
+   })
+
+   newsEventsList3.forEach(edge => {
+     createPage({
+       type: 'NewsEvent',
+       match: '/news-events/:uid',
+       path: `/news-events/${edge.node._meta.uid}`,
+       component: newsEventsTemplate,
+       context: {
+         uid: edge.node._meta.uid,
+       },
+     })
+   })
+
+  newsEventsList4.forEach(edge => {
+     createPage({
+       type: 'NewsEvent',
+       match: '/news-events/:uid',
+       path: `/news-events/${edge.node._meta.uid}`,
+       component: newsEventsTemplate,
+       context: {
+         uid: edge.node._meta.uid,
+       },
+     })
+   })
+
+    newsEventsList5.forEach(edge => {
+     createPage({
+       type: 'NewsEvent',
+       match: '/news-events/:uid',
+       path: `/news-events/${edge.node._meta.uid}`,
+       component: newsEventsTemplate,
+       context: {
+         uid: edge.node._meta.uid,
+       },
+     })
+   }) 
 
   testimonialList.forEach(edge => {
     createPage({
@@ -109,3 +222,197 @@ exports.createPages = async ({ graphql, actions }) => {
   })
 
 }
+
+/*
+
+exports.createPages = async ( {actions, graphql, reporter} ) => {
+  const { createPage } = actions;
+
+  const newsResult = await graphql(`
+      allNews_and_eventss {
+        edges {
+          node {
+            title 
+            _meta {
+              uid
+            }
+          }
+        }
+      }
+  `)
+
+  const newsEvents = newsResult.data.prismic.allNews_and_eventss.edges
+  const newsEventsTemplate = require.resolve(`./src/templates/newsEvents.js`)
+
+  newsEvents.forEach(edge => {
+    createPage({
+      type: 'NewsEvent',
+      match: '/news-events/:uid',
+      path: `/news-events/${edge.node._meta.uid}`,
+      component: newsEventsTemplate,
+      context: {
+        uid: edge.node._meta.uid,
+      }
+    })
+  })
+
+}
+
+
+
+*/
+
+
+
+
+/*
+
+
+
+
+//https://github.com/gatsbyjs/gatsby/issues/13589
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  //set 'after' to nil
+  let after = null
+
+  //set first $limit to  9
+  const limit = 1
+
+  //set keepQuerying to true
+  let keepQuerying = true
+
+  //accumulate news in this array
+  let allNewsPosts = []
+
+  //while loop graphql
+  //need first {limit}, after {lastcursor = ''} 
+    //return uid
+    //allNewsPosts = allNewsPosts.concat(DATA from query)
+    //set after to lastcursor
+    //if nextcursor === ''
+    //then break loop
+
+  const newsNum = await graphql(`
+    query {
+      prismic {
+        allNews_and_eventss {
+          totalCount
+        }
+      }
+    }
+  `)
+
+  while (i < newsNum) {
+    i++
+    const result = await graphql(`
+      query NewsPosts($limit: Int! = 1) {
+        prismic {
+          allNews_and_eventss(first: $limit) {
+            totalCount
+            edges {
+              node {
+                _meta {
+                  uid
+                }
+              }
+              cursor
+            }
+            pageInfo {
+              startCursor
+              endCursor
+              hasNextPage
+              hasPreviousPage
+            }
+          }
+
+        }
+      }
+    `)
+    
+    reporter.info(result.data.prismic.allNews_and_eventss.edges[0].node._meta.uid)
+    reporter.info(`Has next page: ${result.data.prismic.allNews_and_eventss.pageInfo.hasNextPage}`)
+    // reporter.info(`UID is: ${result.data.prismic.allNews_and_eventss.edges[0].node._meta.uid}`)
+    allNewsPosts = allNewsPosts.concat(result.data.prismic.allNews_and_eventss.edges.node)
+
+    // if (result.data.prismic.allNews_and_eventss.pageInfo.hasNextPage === true) {
+    //   keepQuerying = false
+    // }
+
+    //after = result.data.prismic.allNews_and_eventss.pageInfo.node.endCursor
+    // reporter.info(after)
+    //reporter.info(allNewsPosts.length)
+
+    // if (result.data.prismic.allNews_and_eventss.totalCount < limit) {
+    //   keepQuerying = false
+    // }
+  }
+
+  //createPage using data from allNewsPosts  
+  allNewsPosts.forEach(newsPost => {
+    actions.createPage({
+      type: 'NewsEvent',
+      match: '/news-events/:uid',
+      path: `/news-events/${newsPost._meta.uid}`,
+      component: newsEventsTemplate,
+      context: {
+        uid: newsPost.node._meta.uid,
+      },
+    })
+
+  })
+}
+
+
+  // newsEventsList.forEach(edge => {
+  //   createPage({
+  //     type: 'NewsEvent',
+  //     match: '/news-events/:uid',
+  //     path: `/news-events/${edge.node._meta.uid}`,
+  //     component: newsEventsTemplate,
+  //     context: {
+  //       uid: edge.node._meta.uid,
+  //     },
+  //   })
+  // })
+
+
+    //   const result = await graphql(`
+    //   query NewsPosts($limit: Int! = 1, $after: String) {
+    //     prismic {
+    //       allNews_and_eventss(first: $limit, after: $after) {
+    //         totalCount
+    //         edges {
+    //           node {
+    //             _meta {
+    //               uid
+    //             }
+    //           }
+    //           cursor
+    //         }
+    //         pageInfo {
+    //           startCursor
+    //           endCursor
+    //           hasNextPage
+    //           hasPreviousPage
+    //         }
+    //       }
+
+    //     }
+    //   }
+    // `)
+
+
+  //     allNewsPosts.forEach(newsPost => {
+  //   actions.createPage({
+  //     type: 'NewsEvent',
+  //     match: '/news-events/:uid',
+  //     path: `/news-events/${newsPost._meta.uid}`,
+  //     component: newsEventsTemplate,
+  //     context: {
+  //       uid: newsPost.node._meta.uid,
+  //     },
+  //   })
+
+  // })
+
+  */
--- src/utils/link-resolver.js
+++ src/utils/link-resolver.js
@@ -1,4 +1,13 @@
-const { registerLinkResolver } = require("gatsby-source-prismic-graphql")
-const { linkResolver } = require("./src/utils/linkResolver")
+// const { registerLinkResolver } = require("gatsby-source-prismic-graphql")
+// const { linkResolver } = require("./src/utils/linkResolver")
 
-registerLinkResolver(linkResolver)
+// registerLinkResolver(linkResolver)
+
+module.exports = {
+  linkResolver(doc) {
+    if (doc.type == 'NewsEvent') {
+      return `/news-events/${doc.uid}`;
+    }
+    return '/';
+  },
+}
